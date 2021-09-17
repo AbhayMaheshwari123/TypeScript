464b83e8781c0185f0daae54ee79e2423c3ced8d
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-09-17 16:04:42 +0530

    Adding html and css file

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var char = 'apple';
+console.log(char);
+var func = function (dia) {
+    console.log(dia * Math.PI);
+};
+func(20);
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+const char='apple'
+
+console.log(char)
+
+
+const func=(dia:number)=>{
+    console.log(dia* Math.PI)
+}       
+
+func(20)
